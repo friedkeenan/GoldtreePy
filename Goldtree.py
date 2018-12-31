a40225d77d91493de44c5072feb21f4afbe45971
friedkeenan <friedkeenan@protonmail.com>
2018-12-31 11:25:32 -0600

    Add files via upload

---
+++ Goldtree.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import usb.core
+import usb.util
+import struct
+import sys
+import os
+
+from PFS0 import PFS0
+
+def get_switch():
+    dev=usb.core.find(idVendor=0x057e, idProduct=0x3000)
+    if dev is None:
+        raise ValueError("Device not found")
+    return dev
+def get_ep(dev):
+    dev.set_configuration()
+    intf=dev.get_active_configuration()[(0,0)]
+    return (usb.util.find_descriptor(intf,
+            custom_match=lambda e:usb.util.endpoint_direction(e.bEndpointAddress)==usb.util.ENDPOINT_OUT),
+            usb.util.find_descriptor(intf,
+            custom_match=lambda e:usb.util.endpoint_direction(e.bEndpointAddress)==usb.util.ENDPOINT_IN))
+
+class CommandId:
+    ConnectionRequest=0
+    ConnectionResponse=1
+    NSPName=2
+    Start=3
+    NSPData=4
+    NSPContent=5
+    NSPTicket=6
+    NSPCert=7
+    Finish=8
+
+class Command:
+    GLUC=0x43554c47
+    def __init__(self,cmd_id=0,raw=None):
+        if raw is None:
+            self.cmd_id=cmd_id
+            self.magic=self.GLUC
+        else:
+            self.magic,self.cmd_id=struct.unpack("=II",raw)
+    def magic_ok(self):
+        return self.magic==self.GLUC
+    def has_id(self,cmd_id):
+        return self.cmd_id==cmd_id
+    def __bytes__(self):
+        return struct.pack("=II",self.magic,self.cmd_id)
+
+dev=get_switch()
+ep=get_ep(dev)
+def write(buffer):
+    ep[0].write(buffer)
+def read(length):
+    return ep[1].read(length).tobytes()
+
+invalid_cmd="An invalid command was received. Are you sure Goldleaf is active?"
+install_cancelled="Goldleaf has canceled the installation."
+
+def main():
+    c=Command()
+    write(bytes(c))
+    c=Command(raw=read(8))
+    if c.magic_ok():
+        if c.has_id(CommandId.ConnectionResponse):
+            print("Connection was established with Goldleaf.")
+            c=Command(CommandId.NSPName)
+            write(bytes(c))
+            base_name=os.path.basename(sys.argv[1])
+            write(struct.pack("=I",len(base_name)))
+            write(base_name.encode())
+            print("NSP name sent to Goldleaf")
+            resp=None
+            while resp is None:
+                try:
+                    resp=read(8)
+                    c=Command(raw=resp)
+                except usb.core.USBError:
+                    pass
+            if c.magic_ok():
+                if c.has_id(CommandId.Start):
+                    print("Goldleaf is ready for the installation. Preparing everything...")
+                    pnsp=PFS0(sys.argv[1])
+                    c=Command(CommandId.NSPData)
+                    write(bytes(c))
+                    write(struct.pack("=I",len(pnsp.files)))
+                    tik_idx=-1
+                    cert_idx=-1
+                    tmp_idx=0
+                    for file in pnsp.files:
+                        write(struct.pack("=I",len(file.name)))
+                        write(file.name.encode())
+                        write(struct.pack("=Q",pnsp.header_size+file.file_offset))
+                        write(struct.pack("=Q",file.file_size))
+                        if os.path.splitext(file.name)[1][1:].lower()=="tik":
+                            tik_idx=tmp_idx
+                        elif os.path.splitext(file.name)[1][1:].lower()=="cert":
+                            cert_idx=tmp_idx
+                        tmp_idx+=1
+                    while True:
+                        c=Command(raw=read(8))
+                        if c.magic_ok():
+                            if c.has_id(CommandId.NSPContent):
+                                idx=struct.unpack("=I",read(4))[0]
+                                print("Sending content '"+pnsp.files[idx].name+"'... ("+str(idx+1)+" of "+str(len(pnsp.files))+")")
+                                for buf in pnsp.read_chunks(idx):
+                                    write(buf)
+                                print("Content was sent to Goldleaf.")
+                            elif c.has_id(CommandId.NSPTicket):
+                                print("Sending ticket file...")
+                                write(pnsp.read_file(tik_idx))
+                            elif c.has_id(CommandId.NSPCert):
+                                print("Sending certificate file...")
+                                write(pnsp.read_file(cert_idx))
+                            elif c.has_id(CommandId.Finish):
+                                break
+                        else:
+                            print(invalid_cmd)
+                            return 1
+                elif c.has_id(CommandId.Finish):
+                    print(install_cancelled)
+                else:
+                    print(invalid_cmd)
+                    return 1
+            else:
+                print(invalid_cmd)
+                return 1
+        elif c.has_id(CommandId.Finish):
+            print(install_cancelled)
+        else:
+            print(invalid_cmd)
+            return 1
+    else:
+        print(invalid_cmd)
+        return 1
+    print("The installation has finished.")
+    #c=Command(CommandId.Finish)
+    #write(bytes(c))
+    return 0
+
+if __name__=="__main__":
+    sys.exit(main())
+
---
+++ PFS0.py
@@ -0,0 +1,38 @@
+import struct
+
+class PFS0:
+    class FileEntry:
+        def __init__(self,data):
+            self.file_offset=struct.unpack("=Q",data[:0x8])[0]
+            self.file_size=struct.unpack("=Q",data[0x8:0x10])[0]
+            self.name_offset=struct.unpack("=I",data[0x10:0x14])[0]
+            self.name=None
+    def __init__(self,filename):
+        self.filename=filename
+        if self.read_raw(0x0,0x4)!=b"PFS0":
+            raise ValueError("File is not a PFS0")
+        num_files=struct.unpack("=I",self.read_raw(0x4,0x8))[0]
+        len_strings=struct.unpack("=I",self.read_raw(0x8,0xC))[0]
+        self.files=[PFS0.FileEntry(self.read_raw(0x10+0x18*x,0x10+0x18*(x+1))) for x in range(num_files)]
+        self.header_size=0x10+0x18*num_files
+        file_names=self.read_raw(self.header_size,self.header_size+len_strings).split(b"\0")[:num_files]
+        for i in range(num_files):
+            self.files[i].name=file_names[i].decode()
+        self.header_size+=len_strings
+    def read_raw(self,start,end):
+        with open(self.filename,"rb") as f:
+            f.seek(start)
+            data=f.read(end-start)
+        return data
+    def read_file(self,idx):
+        file_entry=self.files[idx]
+        return self.read_raw(self.header_size+file_entry.file_offset,self.header_size+file_entry.file_offset+file_entry.file_size)
+    def read_chunks(self,idx,chunk_size=0x100000):
+        file_entry=self.files[idx]
+        to_read=file_entry.file_size
+        cur_offset=self.header_size+file_entry.file_offset
+        while to_read>0:
+            tor=min(chunk_size,to_read)
+            yield self.read_raw(cur_offset,cur_offset+tor)
+            cur_offset+=tor
+            to_read-=tor
