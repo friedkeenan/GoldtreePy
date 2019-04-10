9166ba4f622ff496c8b642dc5c03b471d03ef41c
rdrooij <rdrooij@sp.nl>
2019-04-10 20:47:50 +0200

    Fix missing whitespace after comma

--- Goldtree.py
+++ Goldtree.py
@@ -18,7 +18,7 @@ def get_switch():
 
 def get_ep(dev):
     dev.set_configuration()
-    intf = dev.get_active_configuration()[(0,0)]
+    intf = dev.get_active_configuration()[(0, 0)]
     return (usb.util.find_descriptor(intf,
             custom_match=lambda e:usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_OUT),
             usb.util.find_descriptor(intf,
@@ -39,22 +39,22 @@ class CommandId:
 class Command:
     GLUC = b"GLUC"
 
-    def __init__(self,cmd_id=0,raw=None):
+    def __init__(self, cmd_id=0, raw=None):
         if raw is None:
             self.cmd_id = cmd_id
             self.magic = self.GLUC
         else:
             self.magic = raw[:4]
-            self.cmd_id = struct.unpack("<I",raw[4:])[0]
+            self.cmd_id = struct.unpack("<I", raw[4:])[0]
     def magic_ok(self):
         return self.magic == self.GLUC
-    def has_id(self,cmd_id):
+    def has_id(self, cmd_id):
         return self.cmd_id == cmd_id
     def __bytes__(self):
-        return self.magic+struct.pack("<I",self.cmd_id)
+        return self.magic+struct.pack("<I", self.cmd_id)
     def write(self):
         write(self.magic)
-        write(struct.pack("<I",self.cmd_id))
+        write(struct.pack("<I", self.cmd_id))
     @staticmethod
     def read():
         return Command(raw=read(4)+read(4))
@@ -64,12 +64,12 @@ dev = get_switch()
 ep = get_ep(dev)
 
 
-def write(buffer,timeout=3000):
-    ep[0].write(buffer,timeout=timeout)
+def write(buffer, timeout=3000):
+    ep[0].write(buffer, timeout=timeout)
 
 
-def read(length,timeout=3000):
-    return ep[1].read(length,timeout=timeout).tobytes()
+def read(length, timeout=3000):
+    return ep[1].read(length, timeout=timeout).tobytes()
 
 
 invalid_cmd = "An invalid command was received. Are you sure Goldleaf is active?"
@@ -86,7 +86,7 @@ def main():
             c = Command(CommandId.NSPName)
             c.write()
             base_name = os.path.basename(sys.argv[1])
-            write(struct.pack("<I",len(base_name)))
+            write(struct.pack("<I", len(base_name)))
             write(base_name.encode())
             print("NSP name sent to Goldleaf")
             while True:
@@ -101,14 +101,14 @@ def main():
                     pnsp = PFS0(sys.argv[1])
                     c = Command(CommandId.NSPData)
                     c.write()
-                    write(struct.pack("<I",len(pnsp.files)))
+                    write(struct.pack("<I", len(pnsp.files)))
                     tik_idx = -1
                     tmp_idx = 0
                     for file in pnsp.files:
-                        write(struct.pack("<I",len(file.name)))
+                        write(struct.pack("<I", len(file.name)))
                         write(file.name.encode())
-                        write(struct.pack("<Q",pnsp.header_size+file.file_offset))
-                        write(struct.pack("<Q",file.file_size))
+                        write(struct.pack("<Q", pnsp.header_size+file.file_offset))
+                        write(struct.pack("<Q", file.file_size))
                         if os.path.splitext(file.name)[1][1:].lower() == "tik":
                             tik_idx = tmp_idx
                         tmp_idx += 1
@@ -116,7 +116,7 @@ def main():
                         c = Command.read()
                         if c.magic_ok():
                             if c.has_id(CommandId.NSPContent):
-                                idx = struct.unpack("<I",read(4))[0]
+                                idx = struct.unpack("<I", read(4))[0]
                                 print("Sending content '" + pnsp.files[idx].name + "'... (" + str(idx + 1) + " of " + str(len(pnsp.files)) + ")")
                                 for buf in pnsp.read_chunks(idx):
                                     write(buf)
