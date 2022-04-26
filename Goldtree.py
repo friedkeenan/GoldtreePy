f8a297a434992fcdf4ad4952171c5a6bbc71dea9
friedkeenan <friedkeenan@protonmail.com>
2022-04-26 17:19:22 -0400

    Update for newer pak

--- Goldtree.py
+++ Goldtree.py
@@ -55,7 +55,9 @@ class GoldtreeContext(pak.PacketContext):
 class GoldtreePacket(pak.Packet):
     pass
 
-class Command(GoldtreePacket, id_type=pak.UInt32):
+class Command(GoldtreePacket):
+    id_type = pak.UInt32
+
     MAGIC = b"GLCI"
 
 class Result(enum.Enum):
